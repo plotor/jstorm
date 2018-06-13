75715af63d9cd8ad1236bcc6121a57ad3b7b740e
zhenchao <zhenchao.wang@hotmail.com>
2018-06-13 23:33:17 +0800

    阅读线程模型

--- jstorm-core/src/test/java/com/alibaba/jstorm/callback/MyRunnableCallback.java
+++ jstorm-core/src/test/java/com/alibaba/jstorm/callback/MyRunnableCallback.java
@@ -1,18 +1,22 @@
 package com.alibaba.jstorm.callback;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * @author zhenchao.wang 2018-06-13 22:14
  * @version 1.0.0
  */
 public class MyRunnableCallback extends RunnableCallback {
 
+    private static AtomicInteger count = new AtomicInteger();
+
     @Override
     public void run() {
-        System.out.println("Thread-" + Thread.currentThread().getId() + " is running.");
+        System.out.println("[" + count.incrementAndGet() + "] thread-" + Thread.currentThread().getId() + " is running.");
     }
 
     @Override
     public Object getResult() {
-        return 3;
+        return 1;
     }
 }
